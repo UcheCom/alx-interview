a26628119d8b8e04131f8cfc8e194d3f161dd97a
Uchenna Oko <uche4newton1981@gmail.com>
2024-04-16 10:44:52 +0100

    N queens error fixed

--- 0x05-nqueens/0-nqueens.py
+++ 0x05-nqueens/0-nqueens.py
@@ -2,13 +2,12 @@
 """Module solves N queens problem"""
 import sys
 
-
 if len(sys.argv) > 2 or len(sys.argv) < 2:
     print("Usage: nqueens N")
     exit(1)
 
 if not sys.argv[1].isdigit():
-    print("N must be a numbe")
+    print("N must be a number")
     exit(1)
 
 if int(sys.argv[1]) < 4:
