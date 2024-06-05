07e25a62e492c5281cc8b2205d5baa1b6773c44b
Uchenna Oko <uche4newton1981@gmail.com>
2024-06-05 11:49:56 +0100

    Updated

--- x07-rotate_2d_matrix/0-rotate_2d_matrix.py
+++ x07-rotate_2d_matrix/0-rotate_2d_matrix.py
@@ -11,9 +11,11 @@ def rotate_2d_matrix(matrix):
     x = len(matrix)
 
     # Transpose the matrix
-    for i in range(x):
-        for j in range(i, x):
-            matrix[i][j], matrix[j][i] = matrix[j][i], matrix[i][j]
+    for i in range(0, x):
+        for j in range(i + 1, x):
+            tmp = matrix[i][j]
+            matrix[i][j] = matrix[j][i]
+            matrix[j][i] = tmp
 
     for i in range(x):
         matrix[i].reverse()
